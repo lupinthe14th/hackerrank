88989c79f9cadac37056c27de504fe17cf76ef76
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-09-26 15:28:39 +0900

    improve(30-binary-numbers): add test case

--- domains/tutorials/30-days-of-code/30-binary-numbers/main_test.go
+++ domains/tutorials/30-days-of-code/30-binary-numbers/main_test.go
@@ -15,6 +15,11 @@ var cases = []struct {
 	{id: 3, input: 100000, want: 2},
 	{id: 4, input: 99999, want: 5},
 	{id: 5, input: 439, want: 3},
+	{id: 6, input: 951, want: 3},
+	{id: 7, input: 1911, want: 3},
+	{id: 8, input: 65535, want: 16},
+	{id: 9, input: 524283, want: 16},
+	{id: 10, input: 524275, want: 15},
 }
 
 func TestBinaryNumber(t *testing.T) {
